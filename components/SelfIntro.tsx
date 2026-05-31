66d2f68733d211440ed8167c04c860896a3a065d
jim0128 <jimjobshk@gmail.com>
2026-06-01 01:35:43 +0800

    Update SelfIntro.tsx with new desctiption

--- components/SelfIntro.tsx
+++ components/SelfIntro.tsx
@@ -11,9 +11,9 @@ const SelfIntro: NextPage = () => {
         {/* self intro */}
         <div className="w-full md:w-2/5 text-slate-50	text-2xl md:p-4 p-10">
           <div className="pb-4 text-3xl font-bold text-slate-300 md:pb-8">
-            Hi, This is Jim Chan
+            Hi, I am Jim Chan
           </div>
-          A software in Hong Kong who love to build web things.
+          A software engineer in Hong Kong who love to build web things.
         </div>
         {/* image part */}
         <div className="w-4/5 mx-auto md:w-3/5 md:p-0 sm:w-full sm:p-28">
